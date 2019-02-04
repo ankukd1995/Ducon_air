bcaadabddb41853a127c60498d56103313e1b24f
unknown <ankukd1995@gmail.com>
2019-02-04 12:55:55 +0530

    Write the script for the Chnage password field

--- src/Automation/Account.java
+++ src/Automation/Account.java
@@ -1,6 +1,7 @@
 package Automation;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
@@ -45,7 +46,7 @@ public class Account
 	  	       Thread.sleep(2000);  
 	  	       
 	  	       //going into the edit mode of the
-	  	       driver.findElement(By.className("btn-third edit-btn")).click();
+	  	       driver.findElement(By.xpath("//div[@class='col-12 top-heading']//a[@class='btn-third edit-btn'][contains(text(),'EDIT')]")).click();
 	  	       Thread.sleep(2000);  
 	  	       
 	  	       // Edting the first name last name
@@ -54,20 +55,49 @@ public class Account
 	  	      Thread.sleep(1000); 
 	  	      driver.findElement(By.xpath("//input[@id='first_name']")).sendKeys("Ankush");
 	  	      driver.findElement(By.xpath("//input[@id='last_name']")).sendKeys("Pawar");
-	  	      driver.findElement(By.xpath("//input[@id='mobile_number']")).sendKeys("4456453346"); 
-	  	      Thread.sleep(1000); 
-	  	     driver.findElement(By.xpath("//a[@id='js-profile-save-btn']")).sendKeys("Ankush");  
-	  	     Thread.sleep(1000); 
+	  	      Thread.sleep(2000); 
+	  	    
 	  	     
+	  	     // Clicking on the save button
+	  	     driver.findElement(By.xpath("//a[@id='js-profile-save-btn']")).click();
+	  	    Thread.sleep(2000);  
 	  	     // Editing the profile for the change password
-	  	      driver.findElement(By.className("btn-third edit-btn")).click();
+	  	      driver.findElement(By.xpath("//div[@class='col-12 top-heading']//a[@class='btn-third edit-btn'][contains(text(),'EDIT')]")).click();
   	          Thread.sleep(2000);  
+  	   
+  	          Actions a = new Actions(driver);
+  	          a.sendKeys(Keys.PAGE_DOWN).build().perform();
   	          
+  	         Thread.sleep(2000);
   	          // Clicking on the change passowrd link
+  	          driver.findElement(By.xpath("//a[@class='change--pwd']")).click();
+  	          Thread.sleep(1000);
   	          
-	  	   
-	  	       
-		  
+  	          // clicking on the change password button'
+  	          driver.findElement(By.xpath("//button[@id='js-change-user-pass']")).click();
+  	          
+  	          // Printing the error message
+  	          String e = driver.findElement(By.xpath("//span[@id='error_old_pass']")).getText();
+  	          String b = driver.findElement(By.xpath("//span[@id='error_new_pass']")).getText();
+  	          String c = driver.findElement(By.xpath("//span[@id='error_confirm_pass']")).getText();
+  	          
+  	          System.out.println("old password error message:" + e);
+  	          System.out.println("old password error message:" +b);
+  	           System.out.println("old password error message:" + c);	
+  	         Thread.sleep(2000);  
+  	  	   
+  	         
+  	           // Catching the old password wrong error
+  	         
+  	         driver.findElement(By.xpath("//input[@id='user_old_pass']")).sendKeys("363574sdgssdfy");
+  	         driver.findElement(By.xpath("//input[@id='user_new_pass']")).sendKeys("123456"); 
+  	         driver.findElement(By.xpath("//input[@id='user_confirm_pass']")).sendKeys("123456");
+  	         Thread.sleep(2000);  
+  	         driver.findElement(By.xpath("//button[@id='js-change-user-pass']")).click();
+  	         Thread.sleep(1000);  
+  	         String g = driver.findElement(By.xpath("//span[@id='error_old_pass']")).getText();
+  	         System.out.println("old password wrong error message:" + g);
+	      		  
 	}
 	
 }
